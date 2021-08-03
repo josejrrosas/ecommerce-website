759545a0f7e82ccf21a4b6756cfa86bf655ad4c5
Jose Rosas <josejrrosas@yahoo.com>
2021-08-02 19:53:25 -0500

    comepleted tag model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const Product = require('./Product.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
