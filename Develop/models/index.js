6f220abb622911a3907bd452bf86c7707cd107b3
Jose Rosas <josejrrosas@yahoo.com>
2021-08-02 20:28:11 -0500

    comepleted index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,39 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { hasMany } = require('./Product');
 
+//----------------------------------------
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 
+//----------------------------------------
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
 
+//----------------------------------------
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: {
+    model: ProductTag
+  },
+  foreignKey: 'product_id'
+});
 
+//----------------------------------------
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: {
+    model: ProductTag
+  },
+  foreignKey: 'tag_id'
+});
+
+//----------------------------------------
 
 module.exports = {
   Product,
